f15ddfd5750e72fd2b51c36b6cadf22e3d6a90cb
Flamur Bakiu <52135865+flamurbakiu@users.noreply.github.com>
2023-09-09 12:43:03 +0200

    Implementation of Auth Page (#4)

--- src/index.js
+++ src/index.js
@@ -21,6 +21,14 @@ i18n
           "Sign_Up": "Sign Up",
           "Footer_Text_1": "Copyright",
           "Footer_Text_2": "Restaurants. All Rights Reserved.",
+          "Your_FirstName": "Your First Name",
+          "Your_LastName": "Your Last Name",
+          "Your_Phone_Number": "Your Phone Number",
+          "Your_Email": "Your Email",
+          "Your_Password": "Your Password",
+          "Create_Account": "Create Account",
+          "Create_New_Account": "Create new account",
+          "Login_With_Existing_Account": "Login with existing account",
         }
       },
       mk: {
@@ -33,6 +41,14 @@ i18n
           "Profile": "Профил",
           "Footer_Text_1": "Авторски права",
           "Footer_Text_2": "Мајстори. Сите права се задржани.",
+          "Your_FirstName": "Вашето име",
+          "Your_LastName": "Вашето презиме",
+          "Your_Phone_Number": "Вашиот телефонски број",
+          "Your_Email": "Вашата електронска пошта",
+          "Your_Password": "Вашата лозинка",
+          "Create_Account": "Креирај сметка",
+          "Create_New_Account": "Креирај нова сметка",
+          "Login_With_Existing_Account": "Најавете се со постоечка сметка",
         }
       },
       sq: {
@@ -45,6 +61,14 @@ i18n
           "Sign_Up": "Regjistrohu",
           "Footer_Text_1": "E drejta e autorit",
           "Footer_Text_2": "Majstori. Të gjitha të drejtat e rezervuara.",
+          "Your_FirstName": "Emri juaj",
+          "Your_LastName": "Mbiemri juaj",
+          "Your_Phone_Number": "Numri juaj i telefonit",
+          "Your_Email": "Email-i juaj",
+          "Your_Password": "Fjalëkalimi juaj",
+          "Create_Account": "Krijo llogari",
+          "Create_New_Account": "Hapni një llogari të re",
+          "Login_With_Existing_Account": "Hyni me llogarinë ekzistuese",
         }
       }
     },
