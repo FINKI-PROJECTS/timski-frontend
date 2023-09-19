3dc06db5d0b445488db033e23d356ef754be2a54
jetonramadani <jeton.ramadani@students.finki.ukim.mk>
2023-09-19 17:52:58 +0200

    Finished payment itnegration

--- src/components/Services/ServiceDetails.js
+++ src/components/Services/ServiceDetails.js
@@ -1,10 +1,11 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { Link, useLocation, useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 import styles from "./ServiceDetails.module.scss";
 import Services from "../Services/Services";
 import image from "../../assets/home-banner-bg.jpg";
 import { Container } from "../layout/Container";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/auth-context";
 
 const fourLastServices = [
   {
@@ -59,8 +60,10 @@ const fourLastServices = [
 
 const ServiceDetails = () => {
   const { serviceId } = useParams();
+  const authCtx = useContext(AuthContext);
   const [service, setService] = useState({});
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     if (!serviceId) {
@@ -68,6 +71,12 @@ const ServiceDetails = () => {
       navigate("/");
       return;
     }
+    if (location.search?.includes("success=true")) {
+      alert("Payment successfull.");
+      navigate("/service/" + serviceId);
+      return;
+    }
+
     const url = process.env.REACT_APP_API_URL + "posts/getById/" + serviceId;
     fetch(url, {
       method: "GET",
@@ -92,8 +101,35 @@ const ServiceDetails = () => {
         navigate("/");
         alert(err.message)
       });
+
   }, [])
 
+  const getPaymentLink = async () => {
+    await fetch(process.env.REACT_APP_API_URL + `payment/get-link?serviceId=${serviceId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": 'Bearer ' + authCtx.token.access,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            throw new Error(data?.message);
+          });
+        }
+      })
+      .then(data => {
+        window.location = data.paymentURL;
+      })
+      .catch((err) => {
+        navigate("/");
+        alert(err.message)
+      });
+
+  }
   return (
     <div className={styles.serviceDetails}>
       <div className={styles["service-details-banner"]}>
@@ -107,6 +143,7 @@ const ServiceDetails = () => {
               <span style={{ backgroundColor: 'rgb(217, 237, 86)' }}>{service.category}</span>
               <span style={{ backgroundColor: '#81c6db' }}>User: {service?.createdUser?.fullName}</span>
               <span>Price: {service.price} den</span>
+              <button className={styles["button-ripple"]} onClick={getPaymentLink}>Pay</button>
             </div>
           </div>
           <div className={styles.description}>
